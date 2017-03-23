e9451ffa4979781218f42a2dbb6c2ba6f8ecf609
Tom <tom@tomforb.es>
2017-03-23 20:00:08 +0000

    Initial release

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ setup(
     name='spamfinder',
     version='0.1',
     packages=['spam'],
-    url='',
+    url='https://github.com/orf/spam/',
     license='',
     author='orf',
     author_email='tom@tomforb.es',
     description='Find the most common senders in your inbox',
     entry_points={
         'console_scripts': [
-            'spam = spam.__main__:main'
+            'spam = spam.command:main'
         ]
     },
-    install_requires = [
+    install_requires=[
         'tqdm',
         'ascii-graph',
         'docopt'
